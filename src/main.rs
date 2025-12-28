1120df6a8da83ee5b66d542b40931d7fda18e878
ahmadlgohary <a.elgohary@live.ca>
2025-12-28 23:36:30 +0200

    - implemented  new_line() and  clear_row(). -  created a global static writer with spin lock mutex to allow multiple threads to access the VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,39 @@
-// We are building an independent OS thus we don't include 
+// We are building an independent OS thus we don't include
 // the std library since it relies on OS services
 #![no_std]
 
-// Disable the Rust entry point since we removed the 
+// Disable the Rust entry point since we removed the
 // underlying runtime system and we will define our own entry point
 #![no_main]
 
-// Import our vga printing module
-mod vga_buffer;
-
+mod vga_buffer; // Import our vga printing module
+use core::fmt::Write;
 use core::panic::PanicInfo;
 
+// do not mangle the name of this function
+#[unsafe(no_mangle)] 
 
-#[unsafe(no_mangle)] // do not mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    /*
-    * This is our custom entry point.
-    * The linker looks for a function called `_start` by default
-    * This is why we added the no_mangle attribute
-    */
-
-    vga_buffer::print_something();
-
-    // basic loop to display text on the screen
-    // let vga_buffer = 0xb8000 as *mut u8;
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
+/*
+ * This is our custom entry point.
+ * The linker looks for a function called `_start` by default
+ * This is why we added the no_mangle attribute
+ */
+ pub extern "C" fn _start() -> ! {
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+   
+    write!(
+        vga_buffer::WRITER.lock(),
+        ", some numbers: {} {}",
+        42,
+        1.337
+    )
+    .unwrap();
 
     loop {}
 }
 
-
-#[panic_handler] // our own custom panic handler
+// our own custom panic handler
+#[panic_handler] 
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,7 @@
+use spin::Mutex;
 use volatile::Volatile;
-use core::fmt::{Write, Result};
+use lazy_static::lazy_static;
+use core::fmt::{Result, Write};
 
 const UNKNOWN_CHAR: u8 = 0xfe; // prints ■
 const SPACE_CHAR: u8 = 0x20; // the ' ' char
@@ -94,7 +96,26 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {}
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_WIDTH {
+            for col in 0..BUFFER_HEIGHT {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
 // We are implementing the write_str() in the fmt::Write trait for the Writer Struct
@@ -105,14 +126,10 @@ impl Write for Writer {
     }
 }
 
-pub fn print_something() {
-    let mut writer = Writer {
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(VGA_MEMORY_ADDRESS as *mut Buffer) },
-    };
-    writer.write_byte(b'h');
-    writer.write_string("ello ");
-    writer.write_string("Wörld!");
-    write!(writer, "The numbers are {} and {}",42,1.0/3.0).unwrap();
+    });
 }
