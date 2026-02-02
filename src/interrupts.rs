1c863ecfc5f022413c161ada72afe04b9e8d8fcf
ahmadlgohary <a.elgohary@live.ca>
2026-02-02 06:27:41 +0200

    implemented asynchronous tasks, handling keyboard input asyncrounsly rather than in interrupts, and halting cpu when there is no async tasks

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -6,7 +6,6 @@ use spin;
 use pic8259::ChainedPics;
 use lazy_static::lazy_static;
 
-use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard,ScancodeSet1};
 
 use crate::gdt;
 use crate::{print, println, hlt_loop};
@@ -58,22 +57,10 @@ extern "x86-interrupt" fn timer_interrupt_handler(
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(
     _stack_frame: InterruptStackFrame) {
-        lazy_static! {
-            static ref KEYBOARD: spin::Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = 
-            spin::Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore)
-            );
-        }
-        let mut keyboard = KEYBOARD.lock();
+        
         let mut port = Port::new(0x60);
         let scancode: u8 = unsafe{port.read()};
-        if let Ok(Some(key_event)) = keyboard.add_byte(scancode){
-            if let Some(key) = keyboard.process_keyevent(key_event){
-                match key {
-                    DecodedKey::Unicode(character) => print!("{}", character),
-                    DecodedKey::RawKey(key) => print!("{:?}", key),
-                }
-            }
-        }
+        crate::task::keyboard::add_scancode(scancode);
         unsafe {
             PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8())
         }
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,13 @@
 #![reexport_test_harness_main = "test_main"]
 
 use rust_kernel::println;
+use rust_kernel::task::keyboard;
+// use rust_kernel::task::{Task, simple_executor::SimpleExecutor};
+use rust_kernel::task::Task;
+use rust_kernel::task::executor::Executor;
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
-use x86_64::VirtAddr;
+use x86_64::{structures::paging::Translate, VirtAddr};
 
 extern crate alloc;
 use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
@@ -19,42 +23,37 @@ use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 
 entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    println!("hello again{}\n", "!");
+    println!("Hello World :) \n");
     
     rust_kernel::init();
 
 
+    // ------------------------------------------------------------------
+    // initializing Paging 
+    // ------------------------------------------------------------------
+    
     let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe {rust_kernel::memory::init(physical_memory_offset)};
     let mut frame_allocator = unsafe {
         rust_kernel::memory::BootInfoFrameAllocator::init(&boot_info.memory_map)
     };
+
+    
     rust_kernel::allocator::init_heap(&mut mapper, &mut frame_allocator)
     .expect("heap initialization failed");
-
-    let heap_value = Box::new(67);
-    println!("heap_value at {heap_value:p}");
-    
-    let mut vec = Vec::new();
-    for i in 0..500{
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    let reference_counted = Rc::new(vec![1,2,3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
-
-
 
     #[cfg(test)]
     test_main();
     
-    println!("It did not Crash");
+    println!("\nIt did not Crash");
+    
+    println!("\nAsynchronous Executor is now running!");
+    
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();    
 
-    rust_kernel::hlt_loop();
 }
 
 #[cfg(not(test))]   // our panic handler in normal builds
@@ -68,4 +67,13 @@ fn panic(info: &PanicInfo) -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_kernel::test_panic_handler(info)
+}
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async num {number}")
 }
